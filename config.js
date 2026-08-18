1e9d7902d2a86d4b3083229fd9b065259143f690
Wingman-del <brucekg23@gmail.com>
2026-08-18 15:47:11 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    BOT_NAME: 'MARV-C V1',
+    OWNER_NAME: 'Marv C',
+    OWNER_NUMBER: '254759083715',
+    PREFIX: '.',
+    REPO_LINK: 'https://github.com/yourusername/marv-c-v1',
+    SESSION_FILE: './session.json',
+    ANTI_DELETE: false,
+    TYPING_DELAY: 15000, // 15 seconds
+};
